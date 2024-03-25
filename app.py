7dda4c5262ef83317bd281c25c16a447482edb0f
Eric Reizas <tuo17224@temple.edu>
2024-03-25 17:34:09 -0400

    Used params for call to run

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class App:
             server. See :func:`werkzeug.serving.run_simple` for more
             information.
         """
-        self._app.run()
+        self._app.run(host,port,debug,load_dotenv)
 
     @_app.route('/',methods=["POST","GET"])
     def log_in():
