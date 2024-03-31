e7e47ad466a0804ac6ed7110111c2417eff12af6
Eric Reizas <tuo17224@temple.edu>
2024-03-31 15:31:12 -0400

    CS2STT-24 Began error analysis function

--- game_session.py
+++ game_session.py
@@ -95,8 +95,15 @@ class Game_Session():
         :return: None
         """
         pass
+
+    def analyze_errors(self,freq_mistyped_chars:list[str])->str:
+        """
+        Generates a report for the user based on the letters they frequently mistyped, the hardest letters to type adjacent in sequence to those letters and to each other
+        :param freq_mistyped_chars : list of the three letters the user most frequently mistyped in the game
+        """
+        return f"Your most frequently mistyped letters are {freq_mistyped_chars[0]}, {freq_mistyped_chars[1]}, and {freq_mistyped_chars[2]}."
     
     
 if __name__=="__main__":
     gs = Game_Session(0)
-    print(gs.getWordList())
+    print(gs.analyze_errors(["a","b","c"]))
