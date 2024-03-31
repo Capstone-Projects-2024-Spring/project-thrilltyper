8bb5c6f7bee8a98bb2c3fff98a63b4ac9bbf03e3
Wenjie Gao <tul42337@temple.edu>
2024-03-30 21:28:36 -0400

    CS2STT-12 Added menu page for play link. Individual links not functional.

---
+++ static/js/Menu.js
@@ -0,0 +1,15 @@
+function Menu() {
+    return (
+        <div id="menu-ctn">
+            <h1>
+                Choose Your Game Mode
+            </h1>
+            <div id="menu">
+                <a className="menu-item"><button>Pratice</button></a>
+                <a className="menu-item"><button>Local</button></a>
+                <a className="menu-item"><button>Robot</button></a>
+                <a className="menu-item"><button>Online Match</button></a>
+            </div>
+        </div>
+    );
+}
