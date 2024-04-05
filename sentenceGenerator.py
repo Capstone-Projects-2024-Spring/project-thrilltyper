ab4c28ec35053dcce0e93f64464d44f0a052a5f3
jimmy <jimmyjiang70@gmail.com>
2024-04-05 13:58:49 -0400

    get different sentence from txt file

--- sentenceGenerator.py
+++ sentenceGenerator.py
@@ -1,6 +1,17 @@
+
 import random
 
-with open('easy_sentences.txt', 'r', encoding='utf-8') as file:
-    sentences = file.readlines()
-random_sentence = random.choice(sentences).strip()
-print(random_sentence)
+def get_random_sentence(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        sentences = file.readlines()
+    random_sentence = random.choice(sentences).strip()
+    return random_sentence
+
+# Get random sentences from each file
+easy_random_sentence = get_random_sentence('easy_sentences.txt')
+medium_random_sentence = get_random_sentence('medium_sentences.txt')
+hard_random_sentence = get_random_sentence('hard_sentences.txt')
+
+print("Easy Sentence:", easy_random_sentence)
+print("Medium Sentence:", medium_random_sentence)
+print("Hard Sentence:", hard_random_sentence)
