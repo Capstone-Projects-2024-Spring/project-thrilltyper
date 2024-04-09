40986065bd922fb9522e2b374fa6993314ef4f28
Eric Reizas <tuo17224@temple.edu>
2024-04-09 10:47:26 -0400

    CS2STT-17 Established attrs for helping with difficulty scoring

--- text_generator.py
+++ text_generator.py
@@ -2,6 +2,15 @@ class Text_Generator:
     """
     Responsible for generating text for a game to use and also separating words into different difficulties (the latter is done outside of run time)
     """
+    left_side = "qwert|asdfg|zxcv"
+    left_row2_start = left_side.find('|')
+    left_row3_start = left_side.find('|',left_row2_start+1)
+    right_side = "poiuy|lkjh|mnb"
+    right_row2_start = right_side.find('|')
+    right_row3_start = right_side.find('|',right_row2_start+1)
+    pinkie_chars = "qaz"
+    ring_fing_chars = "wsxopl"
+    
     def getTxtList(self,file) -> list[str]:
         """
         Reads from wordList.txt to create an array of the words in it
