5eebf40097267163eaaa3524c4dd1258db1eac93
Wenjie Chen <tun71931@temple.edu>
2024-04-18 10:09:49 -0400

    initiated method for retrieving highest wpm from database

--- app.py
+++ app.py
@@ -278,6 +278,18 @@ class App:
                 "totalTime" : userData._total_playing_time,
                 "frequentMisTypedWord" : userData._freq_mistyped_words
             })
+        
+    @_app.route('/leaderboard/top_n_highest_wpm/<int:n>', methods=['GET'])
+    def get_top_n_highest_wpm_leaderboard(n):
+        """
+        Retrieve UserData table highest_wpm and converting to json format and send to frontend
+        :param n: an input of the top n number for leaderboard
+        :type n: int
+        :returns: json format leaderboard info 
+        """
+
+        pass
+
 
 class Database:
     """
@@ -818,7 +830,7 @@ if __name__=="__main__":
     #creates database tables and used for testing purposes(insert/update/query/delete)
     with app._app.app_context():
 
-        app.db.drop_all()
+        #app.db.drop_all()
 
         app.db.create_all()
 
@@ -827,7 +839,7 @@ if __name__=="__main__":
         #for example, do not repeat the same number in the num_row as it might have repeated _username and _email (which is suppose to be unique)
         #if you want to re-populate with the same num_rows, you must run app.db.dropall() before this method
         #after testing, you can repeat the number, but preferrably not to do that
-        Database.populate_sample_date(100)
+        #Database.populate_sample_date(10000)
 
         #this method returns a list represention of top-n largest mistyped letters
         # top_n_letters = Database.get_top_n_letters("user35", 6)
