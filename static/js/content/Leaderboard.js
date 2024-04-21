d4a640c7ab64f31031ed4cde13764c0640da8785
Wenjie <tun71931@temple.edu>
2024-04-21 00:26:56 -0400

    more modification to leaderboard styling

--- static/js/content/Leaderboard.js
+++ static/js/content/Leaderboard.js
@@ -4,7 +4,7 @@ function Leaderboard() {
     React.useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('/leaderboard/top_n_highest_wpm/30');
+                const response = await fetch('/leaderboard/top_n_highest_wpm/50');
                 const data = await response.json();
                 setLeaderboardData(data);
             } catch (error) {
@@ -19,17 +19,26 @@ function Leaderboard() {
         <div>
             <h1>Leaderboard</h1>
             <div id="leaderboard">
-                <h2>Top 10 Highest WPM</h2>
+                <h2>Top 50 Highest WPM</h2>
                 <table>
+                    <thead>
+                        <tr>
+                            <th>User Profile Photo</th>
+                            <th>User Name</th>
+                            <th>User WPM</th>
+                        </tr>
+                    </thead>
                     <tbody>
                         {leaderboardData.map((player, index) => (
-                            <tr key={player.username}>
+                            <tr key={player.username} className={index === 0 ? 'rank-one' : index === 1 ? 'rank-two' : index === 2 ? 'rank-three' : ''}>
                                 <td>
                                     <img src={player.profile_photo} alt="Profile" />
                                 </td>
                                 <td className="username">
                                     <strong>{player.username}</strong>
-                                    {index === 0 && <span className="rank-icon"><img src="/static/pics/golden_apple_rank_one.png" alt="Rank 1" /></span>}
+                                    {index === 0 && <span className="rank-icon"><img src="/static/pics/leaderboard_winner.png" alt="Rank 1" /></span>}
+                                    {index === 1 && <span className="rank-icon"><img src="/static/pics/leaderboard_second.png" alt="Rank 2" /></span>}
+                                    {index === 2 && <span className="rank-icon"><img src="/static/pics/leaderboard_third.png" alt="Rank 3" /></span>}
                                 </td>
                                 <td>
                                     {player.highest_wpm} WPM (Rank: {index + 1})
