b12ed18b39a7a292aba4914835a4154d9e98f261
Eric Reizas <tuo17224@temple.edu>
2024-04-30 11:06:58 -0400

    CS2STT-50 Updated UserLetter data after race

--- app.py
+++ app.py
@@ -345,6 +345,10 @@ class App:
                 game_wpm = game_data["wpm"]
                 Database.update(usr,"UserData",_accuracy=(game_data["accuracy"]+float(user_data._accuracy)*num_races)/(num_races+1),_num_races=num_races+1,_total_playing_time=user_data._total_playing_time+game_data["elapsedTime"],_top_wpm=game_wpm if game_wpm>int(user_data._top_wpm) else int(user_data._top_wpm))
                 Database.insert(UserRace,_username=usr,_email=str(user_data._email),_average_wpm=game_wpm,_selected_mode=game_data["mode"],_time_limit=game_data.get("timeLimit"),_date_played=datetime.fromisoformat(game_data["date"]))
+                mistyped_chars = game_data.get("mistypedChars")
+                if mistyped_chars:
+                    #frontend args for the returned json must match those of the db
+                    Database.update(usr,"UserLetter",**mistyped_chars)
             return "Successful"
         return "Not successful"
 
