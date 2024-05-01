3b1bc5d6bfc8ff451940dd961091a60a0415b823
Eric Reizas <tuo17224@temple.edu>
2024-04-30 20:45:48 -0400

    Added rows for new users for db tables

--- app.py
+++ app.py
@@ -149,7 +149,24 @@ class App:
 
                 # Insert user info into the database if doesn"t exists yet
                 if Database.query(uname, "UserInfo") is None:
-                    Database.insert(UserInfo, _username=uname, _password=token["access_token"], _email=token["userinfo"]["email"], _profile_photo=picture)
+                    Database.insert(UserInfo, _username=uname, _password=token["access_token"], _email=uname)
+                    Database.insert(UserData, _username=uname,_email=uname,_accuracy=0,_wins=0,_losses=0,_freq_mistyped_words=0,_total_playing_time=0,_top_wpm=0,_num_races=0,_user_in_game_picture=picture,_last_login_time=datetime.now(timezone.utc))
+                    user_letter_data = {
+                    "_username": uname,
+                    "_email": uname,
+                    **{f"_{letter}": 0 for letter in string.ascii_lowercase},
+                    "_comma": 0,
+                    "_period": 0,
+                    "_exclamation": 0,
+                    "_question": 0,
+                    "_hyphen": 0,
+                    "_semicolon": 0,
+                    "_single_quote": 0,
+                    "_double_quote": 0,
+                    }
+                    Database.insert(UserLetter,**user_letter_data)
+                else:
+                    Database.update(uname,"UserData",_last_login_time=datetime.now(timezone.utc))
             else:
                 # Handle the case where access is denied (user cancelled the login)
                 return "Access denied: Google login was canceled or failed."
