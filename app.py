53a0508d1b9bb20c31dd91ec0a79e1ae06faddfe
Wenjie Gao <tul42337@temple.edu>
2024-05-01 14:44:33 -0400

    Added back avatar for google users

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ class App:
 
                 # Insert user info into the database if doesn"t exists yet
                 if Database.query(uname, "UserInfo") is None:
-                    Database.insert(UserInfo, _username=uname, _password=token["access_token"], _email=uname)
+                    Database.insert(UserInfo, _username=uname, _password=token["access_token"], _email=uname, _profile_photo=picture)
                     Database.insert(UserData, _username=uname,_email=uname,_accuracy=0,_wins=0,_losses=0,_freq_mistyped_words=0,_total_playing_time=0,_top_wpm=0,_num_races=0,_user_in_game_picture=picture,_last_login_time=datetime.now(timezone.utc))
                     user_letter_data = {
                     "_username": uname,
@@ -958,4 +958,4 @@ if __name__=="__main__":
         # top_n_letters = Database.get_top_n_letters("user35", 6)
         # print(top_n_letters)
 
-    app.socketio.run(app._app, host="localhost",debug=True)
+    app.socketio.run(app._app, host="0.0.0.0",debug=True)
